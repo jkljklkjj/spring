62f93fcbdd849c72d05bfafd275bd885599bf5a6
jkljklkjj <2998568539@qq.com>
2024-04-26 23:31:02 +0800

    部分优化

--- src/main/java/com/example/demo/Speech/VoiceToText.java
+++ src/main/java/com/example/demo/Speech/VoiceToText.java
@@ -1,7 +1,10 @@
 package com.example.demo.Speech;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import okhttp3.*;
+import org.w3c.dom.Text;
+
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.FileInputStream;
@@ -34,7 +37,7 @@ public class VoiceToText extends WebSocketListener {
     @Override
     public void onOpen(WebSocket webSocket, Response response) {
         super.onOpen(webSocket, response);
-        new Thread(()->{
+        new Thread(() -> {
             //连接成功，开始发送数据
             int frameSize = 1280; //每一帧音频的大小,建议每 40ms 发送 122B
             int intervel = 40;
@@ -113,5 +116,120 @@ public class VoiceToText extends WebSocketListener {
                 e.printStackTrace();
             }
         }).start();
+    }
+
+    public static class Result {
+        int bg;
+        int ed;
+        String pgs;
+        int[] rg;
+        int sn;
+        Ws[] ws;
+        boolean ls;
+        JsonObject vad;
+
+        public Text getText() {
+            Text text = new Text();
+            StringBuilder sb = new StringBuilder();
+            for (Ws ws : this.ws) {
+                sb.append(ws.cw[0].w);
+            }
+            text.sn = this.sn;
+            text.text = sb.toString();
+            text.sn = this.sn;
+            text.rg = this.rg;
+            text.pgs = this.pgs;
+            text.bg = this.bg;
+            text.ed = this.ed;
+            text.ls = this.ls;
+            text.vad = this.vad == null ? null : this.vad;
+            return text;
+        }
+    }
+
+    public static class Ws {
+        Cw[] cw;
+        int bg;
+        int ed;
+    }
+
+    public static class Cw {
+        int sc;
+        String w;
+    }
+
+    public static class Text {
+        int sn;
+        int bg;
+        int ed;
+        String text;
+        String pgs;
+        int[] rg;
+        boolean deleted;
+        boolean ls;
+        JsonObject vad;
+
+        @Override
+        public String toString() {
+            return "Text{" +
+                    "bg=" + bg +
+                    ", ed=" + ed +
+                    ", ls=" + ls +
+                    ", sn=" + sn +
+                    ", text='" + text + '\'' +
+                    ", pgs=" + pgs +
+                    ", rg=" + Arrays.toString(rg) +
+                    ", deleted=" + deleted +
+                    ", vad=" + (vad == null ? "null" : vad.getAsJsonArray("ws").toString()) +
+                    '}';
+        }
+    }
+
+    //解析返回数据，仅供参考
+    public static class Decoder {
+        private Text[] texts;
+        private int defc = 10;
+
+        public Decoder() {
+            this.texts = new Text[this.defc];
+        }
+
+        public synchronized void decode(Text text) {
+            if (text.sn >= this.defc) {
+                this.resize();
+            }
+            if ("rpl".equals(text.pgs)) {
+                for (int i = text.rg[0]; i <= text.rg[1]; i++) {
+                    this.texts[i].deleted = true;
+                }
+            }
+            this.texts[text.sn] = text;
+        }
+
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            for (Text t : this.texts) {
+                if (t != null && !t.deleted) {
+                    sb.append(t.text);
+                }
+            }
+            return sb.toString();
+        }
+
+        public void resize() {
+            int oc = this.defc;
+            this.defc <<= 1;
+            Text[] old = this.texts;
+            this.texts = new Text[this.defc];
+            for (int i = 0; i < oc; i++) {
+                this.texts[i] = old[i];
+            }
+        }
+
+        public void discard() {
+            for (int i = 0; i < this.texts.length; i++) {
+                this.texts[i] = null;
+            }
+        }
     }
 }
